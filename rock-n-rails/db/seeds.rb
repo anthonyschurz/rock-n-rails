2931c372dc278248eb271bd43894598668ef80b5
anthonyschurz <anthony.schurz@gmail.com>
2016-06-14 16:50:06 -0700

    makes final changes

--- rock-n-rails/db/seeds.rb
+++ rock-n-rails/db/seeds.rb
@@ -1,26 +1,59 @@
-# Wipe the database
 Record.destroy_all
-# Let's create a bunch of records
-Record.create([
-  {
-    title: "On Avery Island",
-    artist: "Neutral Milk Hotel",
-    year: 1996,
-    cover_art: "https://upload.wikimedia.org/wikipedia/en/7/73/On_avery_island_album_cover.jpg",
-    song_count: 12
+records = Record.create([
+       {
+  title: 'The College Dropout',
+  artist: 'Kanye West',
+  year: '2004',
+  cover_art: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Kanyewest_collegedropout.jpg',
+  song_count: '21'
+  },
+       {
+  title: 'Late Registration',
+  artist:'Kanye West',
+  year:'2005',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/f/f4/Late_registration_cd_cover.jpg',
+  song_count:'21',
+  },
+       {
+  title:'Late Orchestration',
+  artist:'Kanye West',
+  year:'2006',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/1/11/Kanye-late-orchestration.jpg',
+  song_count:'13',
+  },
+       {
+  title:'Graduation',
+  artist:'Kanye West',
+  year:'2007',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/7/70/Graduation_(album).jpg',
+  song_count:'13',
+  },
+       {
+  title:'808s & Heartbreak',
+  artist:'Kanye West',
+  year:'2008',
+  cover_art:'https://images-na.ssl-images-amazon.com/images/I/317uUioNjzL._SY300_.jpg',
+  song_count:'12',
   },
   {
-    title: "Everything All the Time",
-    artist: "Band of Horses",
-    year: 2006,
-    cover_art: "https://upload.wikimedia.org/wikipedia/en/5/51/BandofHorsesEverythingalltheTime.jpg",
-    song_count: 10
+  title:'My Beautiful Dark Twisted Fantasy',
+  artist:'Kanye West',
+  year:'2010',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/f/f0/My_Beautiful_Dark_Twisted_Fantasy.jpg',
+  song_count:'13',
   },
   {
-    title: "The Flying Club Cup",
-    artist: "Beirut",
-    year: 2007,
-    cover_art: "https://upload.wikimedia.org/wikipedia/en/4/4c/The_Flying_Club_Cup.jpg",
-    song_count: 13
-  }
+  title:'Yeezus',
+  artist:'Kanye West',
+  year:'2013',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/0/03/Yeezus_album_cover.png',
+  song_count:'10',
+  },
+  {
+  title:'The Life of Pablo',
+  artist:'Kanye West',
+  year:'2016',
+  cover_art:'https://upload.wikimedia.org/wikipedia/en/4/4d/The_life_of_pablo_alternate.jpg',
+  song_count:'19',
+  },
 ])
